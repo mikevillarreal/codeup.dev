f962bdae8bdce4649cec01a60fb3e3f1a495dc6a
Mike Villarreal <sales@moredesignonline.com>
2015-04-08 11:31:02 -0500

    Connecting MySQL Using Constants

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,3 @@
 <?php
-$dbc = new PDO('mysql:host=127.0.0.1;dbname=employees', 'codeup', 'password');
+$dbc = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
--- pdo_test.php
+++ pdo_test.php
@@ -1,3 +1,8 @@
 <?php
-require('db_connect.php');
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME','employees');
+define('DB_USER','codeup');
+define('DB_PASS','password'); 
+
+require_once ('db_connect.php');
 echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
